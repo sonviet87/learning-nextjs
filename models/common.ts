edc79215797b456e2ee59c4fccb6d8a7a6bdd979
devel02 <devel02@iconsys84.com>
2023-07-27 16:48:17 +0700

    add Mui v5

--- models/common.ts
+++ models/common.ts
@@ -1,6 +1,7 @@
 import { NextPage } from "next";
 import { AppProps } from "next/app";
 import { ReactElement, ReactNode } from "react";
+import {EmotionCache} from "@emotion/react";
 
 export interface LayoutProps {
     children: ReactNode
@@ -11,5 +12,6 @@ export type NextPageWithLayout = NextPage & {
 }
 
 export type AppPropsWithLayout = AppProps & {
-    Component: NextPageWithLayout
+    Component: NextPageWithLayout;
+    emotionCache?: EmotionCache
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,27 @@ import { EmptyLayout } from '@/components/layout/empty'
 import { AppPropsWithLayout } from '@/models'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import {createEmotionCache,theme }from '@/utility/index';
 
-export default function App({ Component, pageProps }: AppPropsWithLayout) {
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+
+export default function App({ Component, pageProps, emotionCache = clientSideEmotionCache }: AppPropsWithLayout) {
   const Layout = Component.Layout ?? EmptyLayout
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+      <CacheProvider value={emotionCache}>
+          <ThemeProvider theme={theme}>
+              {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+              <CssBaseline />
+              <Layout>
+                  <Component {...pageProps} />
+              </Layout>
+          </ThemeProvider>
+      </CacheProvider>
   )
 
 }
